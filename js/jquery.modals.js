c0e6233af896de4e017a5d5c85d4bbd47ba90a71
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2017-02-01 23:43:07 +0100

    anchura del main y corrección estilos modal.

--- js/jquery.modals.js
+++ js/jquery.modals.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
 
 $('.js-menu-open').click(function(){
     $(".nav-overlay").addClass("open");
+    $("body").addClass("is-modal-open");
 });
 
 $('.js-menu-close').click(function(){
     $(".nav-overlay").removeClass("open");
+    $("body").addClass("is-modal-open");
 });
 
 });
