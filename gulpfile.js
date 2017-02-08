3b1f45f374815a504222485eee823292271546c1
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2017-02-08 21:49:28 +0100

    Instalación de gulp del y utilizar npm run comando

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
     autoprefixer = require('gulp-autoprefixer'),
     autoprefixerOptions = {browsers: ['last 2 versions', '> 5%', 'Firefox ESR']};
 
+//Transpila base.scss en forma compressed, pasa el autoprefixer y deja el resultado en ./css/base.css
 gulp.task('sassbase', function () {
   return gulp.src('./src/sass/base/base.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -11,7 +13,11 @@ gulp.task('sassbase', function () {
     .pipe(gulp.dest('./css'));
 });
 
+//borra el contenido del directorio './css'
+gulp.task('cleancss', function () {
+  return del(['./css']);
+});
 
-gulp.task('sass', ['sassbase'], function () {
+gulp.task('sass', ['cleancss', 'sassbase'], function () {
   console.log("Generado bundle CSS desde SCSS");
 });
